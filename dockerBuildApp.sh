b9422d5be90397fa980d371201897474dab7baeb
JThirgood <jackson.thirgood@gmail.com>
2021-11-18 16:37:53 +0000

    JT: UPDATED SCRIPT FILE

--- dockerBuildApp.sh
+++ dockerBuildApp.sh
@@ -2,5 +2,7 @@
 # npm install
 npm test
 #docker stop jackson-nodeapp-container || true && docker rm docker-nodeapp-container || true
-docker build -t jackson-nodeapp-image .
+docker build -t jackson-nodeapp-image:${BUILD_NUMBER} .
+docker tag jackson-nodeapp-image:${BUILD_NUMBER}  gcr.io/lbg-training/jackson-nodeapp-image:${BUILD_NUMBER}
+docker push gcr.io/lbg-training/jackson-nodeapp-image:{BUILD_NUMBER}
 #docker run -d -p 5000:9000 --name jackson-nodeapp-container jackson-nodeapp-image
