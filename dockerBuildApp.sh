55cbc162494c308a4a3ce9342ee5e33340e0dc5b
JThirgood <jackson.thirgood@gmail.com>
2021-11-18 15:47:41 +0000

    JT: script fix

--- dockerBuildApp.sh
+++ dockerBuildApp.sh
@@ -2,5 +2,5 @@
 # npm install
 npm test
 #docker stop jackson-nodeapp-container || true && docker rm docker-nodeapp-container || true
-docker build -t gcr.io/lbg-training/jackson-nodeapp-image .
+docker build -t jackson-nodeapp-image .
 #docker run -d -p 5000:9000 --name jackson-nodeapp-container jackson-nodeapp-image
