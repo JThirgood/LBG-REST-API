b5de602c15062a947bef3aeb5231360db9e67d7d
JThirgood <jackson.thirgood@gmail.com>
2021-11-18 15:23:11 +0000

    JT: added gcr details to image

--- dockerBuildApp.sh
+++ dockerBuildApp.sh
@@ -1,6 +1,6 @@
 #!
 # npm install
 npm test
-docker stop jackson-nodeapp-container || true && docker rm docker-nodeapp-container || true
-docker build -t jackson-nodeapp-image .
-docker run -d -p 5000:9000 --name jackson-nodeapp-container jackson-nodeapp-image
+#docker stop jackson-nodeapp-container || true && docker rm docker-nodeapp-container || true
+docker build -t gcr.io/lbg-training/jackson-nodeapp-image .
+#docker run -d -p 5000:9000 --name jackson-nodeapp-container jackson-nodeapp-image
