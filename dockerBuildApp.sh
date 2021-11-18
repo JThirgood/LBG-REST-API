c23ae87284bad2f103143427ca21ed86c0db5864
JThirgood <jackson.thirgood@gmail.com>
2021-11-18 15:21:01 +0000

    JT: edit to dockerBuildApp

--- dockerBuildApp.sh
+++ dockerBuildApp.sh
@@ -1,6 +1,6 @@
 #!
-npm install
+# npm install
 npm test
-docker stop my-node-container-scripted || true && docker rm my-node-container-scripted || true
-docker build -t my-node-image-scripted .
-docker run -d -p 5000:9000 --name my-node-container-scripted my-node-image-scripted
+docker stop jackson-nodeapp-container || true && docker rm docker-nodeapp-container || true
+docker build -t jackson-nodeapp-image .
+docker run -d -p 5000:9000 --name jackson-nodeapp-container jackson-nodeapp-image
