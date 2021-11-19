4bd5ec1668ace4f8a0d9ff8fe9b1f80575732d22
JThirgood <75621151+JThirgood@users.noreply.github.com>
2021-11-19 09:43:52 +0000

    Update dockerBuildApp.sh

--- dockerBuildApp.sh
+++ dockerBuildApp.sh
@@ -5,4 +5,5 @@ docker stop jackson-nodeapp-container || true && docker rm jackson-nodeapp-conta
 docker build -t jackson-nodeapp-image:${BUILD_NUMBER} .
 docker tag jackson-nodeapp-image:${BUILD_NUMBER}  gcr.io/lbg-training/jackson-nodeapp-image:${BUILD_NUMBER}
 docker push gcr.io/lbg-training/jackson-nodeapp-image:${BUILD_NUMBER}
+
 docker run -d -p 5000:9000 --name jackson-nodeapp-container jackson-nodeapp-image:${BUILD_NUMBER}
